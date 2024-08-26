bcde210763eed053533ba198d7271eb10f4e3ff0
net300 <net3.0@outlook.com>
2024-08-26 12:05:14 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -4,5 +4,5 @@ int main()
     print::printchar("hello world");//to print a const char only
     print::printint(1);//to print a number int
     print::printfloat(3.14);//to print a floatnumber
-    print::printend;//the end what to get
+    print::printend();//the end what to get
 }
