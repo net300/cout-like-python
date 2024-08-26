ce3a4df215dd92abc44e4d52416ae52336081fbf
net300 <net3.0@outlook.com>
2024-08-26 12:04:18 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,5 +1,8 @@
 #include 'print.h'
 int main()
 {
-    printchar
+    print::printchar("hello world");//to print a const char only
+    print::printint(1);//to print a number int
+    print::printfloat(3.14);//to print a floatnumber
+    print::printend;//the end what to get
 }
