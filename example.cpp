3c085db49eaeb3d769f2fceb65ee0cde91a54fb2
net300 <net3.0@outlook.com>
2024-08-30 14:13:17 +0800

    the input class is done

--- example.cpp
+++ example.cpp
@@ -1,9 +1,18 @@
-#include "print.h"
+#include "output"
+#include "input"
 int main()
 {
-    print::printchar("hello world");//to print a const char only
-    print::printint(1);//to print a number int
-    print::printfloat(3.14);//to print a floatnumber
-    print::returnline();//return to next line
-    print::printend();//the end what to get
+    output::putchar("hello world");//to print a const char only
+    output::putint(1);//to print a number int
+    output::putfloat(3.14);//to print a floatnumber
+    output::returnline();//return to next line
+    output::putend();//the end what to get
+    input::putchar();//input char
+    //use input::Char to look what char is input
+    input::putint();
+    //use input::Int to look what int is input
+    input::putfloat();
+    //use input::Float to look what float is input
+    input::returnline();//return to next line
+
 }
